b3cd7c2933508de90593c5e594ae6d4ee0fccf3f
DaisyCream@qq.com <DaisyCream@qq.com>
2016-04-14 15:24:42 +0800

    add the js

--- gitStudy/DaisyJs/daisyJs.js
+++ gitStudy/DaisyJs/daisyJs.js
@@ -11,6 +11,7 @@
     Daisy.prototype = {
         constructor : Daisy,
         selector : "",
+        //针对类对象数组，可以像数组一样的输出
         splice : Array.prototype.splice,
         length : 0,
 
@@ -18,13 +19,54 @@
             this.selector = selector;
             this.length = selector.length;
             return this;
-        }
+        },
 
+        /***
+         * event, data, func
+         */
+        bind : function(){
+
+            if(arguments.length == 1){
+                //
+                var events = arguments[0];
+
+                for(var i=0;i<events.length;i++){
+                    addEvent(events[i].key, events);
+                }
+            }else if(arguments.length == 2){
+                //
+                addEvent(arguments[0],arguments[1]);
+            }else{
+                //
+                addEvent()
+            }
+
+            /***
+             * 区分是IE还是其他，IE使用attachEvent
+             * @param type
+             * @param func
+             */
+            function addEvent(type, func){
+                if(window.addEventListener){
+                    this.addEventListener(type, func, false);
+                }else if(window.attachEvent){
+                    this.attachEvent('on' + type, func);
+                }
+            }
+            return this;
+        },
+
+        unbind : function(){
+
+
+        }
 
 
     };
 
     Daisy.prototype.init.prototype = Daisy.prototype;
+
+
 
 
     window.$ = Daisy;

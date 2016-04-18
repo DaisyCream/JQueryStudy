4a5b6331c62eb4874f34725d201b4e46cd4bff21
DaisyCream@qq.com <DaisyCream@qq.com>
2016-04-18 15:55:03 +0800

    add the fun attr text

--- gitStudy/DaisyJs/daisyJs.js
+++ gitStudy/DaisyJs/daisyJs.js
@@ -14,10 +14,12 @@
         //针对类对象数组，可以像数组一样的输出
         splice : Array.prototype.splice,
         length : 0,
+        obj : null,
 
         init : function(selector){
             this.selector = selector;
             this.length = selector.length;
+            this.obj = document.querySelector(selector);
             return this;
         },
 
@@ -42,15 +44,15 @@
             }
 
             /***
-             * 区分是IE还是其他，IE使用attachEvent
+             * 区分是IE8以下用attach，IE使用attachEvent
              * @param type
              * @param func
              */
             function addEvent(type, func){
                 if(window.addEventListener){
-                    this.addEventListener(type, func, false);
+                    this.obj.addEventListener(type, func, false);
                 }else if(window.attachEvent){
-                    this.attachEvent('on' + type, func);
+                    this.obj.attachEvent('on' + type, func);
                 }
             }
             return this;
@@ -62,18 +64,74 @@
                 var events = arguments[0];
 
                 for(var i=0;i<events.length;i++){
-                    addEvent(events[i].key, events);
+                    removeEvent(events[i].key, events);
                 }
 
             }else if(arguments.length == 2){
                 //
-                addEvent(arguments[0],arguments[1]);
+                removeEvent(arguments[0],arguments[1]);
             }else{
                 //
-                addEvent()
+                removeEvent();
             }
 
-        }
+            function removeEvent(type, func){
+                if(window.removeEventListener){
+                    this.obj.removeEventListener(type, func, false);
+                }else if(window.detachEvent){
+                    this.obj.detachEvent('on' + type, func);
+                }
+            }
+            return this;
+        },
+
+        attr : function(attrName, attrValue){
+            if(arguments.length == 1){
+                return this.obj.getAttribute(attrName);
+            }else{
+                this.setAttribute(attrName, attrValue);
+                return this;
+            }
+        },
+
+        html : function(content){
+            if(arguments.length == 0){
+                return getInnerHtml(this.obj);
+            }else {
+                setInnerHtml(this.obj,content);
+            }
+
+            function getInnerHtml(element){
+                return element.innerHTML;
+            }
+
+            function setInnerHtml(element, content){
+                element.innerHTML = content;
+            }
+        },
+
+        text : function(content){
+            if(arguments.length == 0){
+                return getInnerText(this.obj);
+            }else {
+                setInnerText(this.obj,content);
+            }
+
+            function getInnerText(element){
+                return (typeof element.textContent == "string")?
+                    element.textContent:element.innerText;
+            }
+
+            function setInnerText(element,content){
+                if(typeof element.textContent == "string"){
+                    element.content = content;
+                }else{
+                    element.innerText = content;
+                }
+            }
+        },
+
+
 
 
     };
